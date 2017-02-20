866b4fd5a608d08c983d82e70df2d25368a82ec1
daniel <daniel@imap.gmail.com>
2017-02-20 15:52:21 +0300

    Form Creation

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from books.models import Book
 
 # Create your views here.
+
+
+def search_form(request):
+    return render(request,'search_form.html')
+
+
+def search(request):
+    if 'search_box' in request.GET:
+        message="You Searched for: %r " % request.GET['search_box']
+        search_box = request.GET['search_box']
+
+        books = Book.objects.filter(title__icontains=search_box)
+
+        return render(request,'search_results.html',{'books': books,'query': search_box})
+    else:
+        return render(request,'search_form.html', {'error': True})
+
+
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import hello, current_datetime, hours_ahead
+from books import views
 
 urlpatterns = [
     # Examples:
@@ -9,5 +10,7 @@ urlpatterns = [
     url(r'^hello/$',hello),
     url(r'^current/$',current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$',views.search),
     url(r'^admin/', include(admin.site.urls)),
 ]
