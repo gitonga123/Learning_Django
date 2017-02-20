c269dcc662403fce654424ce29888f8c20bcf0d8
daniel <daniel@imap.gmail.com>
2017-02-20 09:42:28 +0300

    Admin Site Configuration

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -7,7 +7,7 @@ class Publisher(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
-    state_provice = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
     country = models.CharField(max_length=50)
     website = models.URLField()
 
@@ -21,7 +21,7 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, verbose_name='e-mail')
 
     def __str__(self):
         return u'%s %s' %(self.first_name,self.last_name)
@@ -31,7 +31,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(null=True,blank=True)
 
 
     def __str__(self):
