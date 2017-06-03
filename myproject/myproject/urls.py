e8bc29bf2d9ed50419b880e533c0b36b41f05855
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-06-03 10:35:48 +0300

    Self Customized Form Validation

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+#from newsletter import views
 
 urlpatterns = [
+    #url(r'^$', newsletter.views.home),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
