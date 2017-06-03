356a383d0f1e8b4737f74ad382a020f336ed5539
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-06-03 13:08:38 +0300

    Form View

--- myproject/newsletter/views.py
+++ myproject/newsletter/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 
 
 def home(request):
 	title="NWL | %s" %("HOME")
+
+	if request.method == "POST":
+		print request.POST
+		
+	form = SignUpForm(request.POST or None)
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		print instance.email
+		print instance.timestamp
+
 	if request.user.is_authenticated():
 		user_logged = "Welcome %s" %(request.user)
 	else:
@@ -13,5 +25,6 @@ def home(request):
 	context = {
 		"template_title" :  title,
 		"headline" : user_logged,
+		"form" : form,
 	}
 	return render(request,"home.html",context)
