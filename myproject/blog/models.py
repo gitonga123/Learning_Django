c12fb17e5bbc0a77afedc6f14579d86b79dc8c56
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-18 13:58:17 +0300

    Migrations, Unit Test, Static Files :simple-smiley:

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics')
+    starter = models.ForeignKey(User, related_name='topics')
+
+    def __str__(self):
+        return self.subject
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_at = models.ForeignKey(User, related_name='posts')
+    updated_at = models.ForeignKey(User, null=True, related_name='+')
+
+    def __str__(self):
+        return self.message
+
