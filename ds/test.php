b519bbc0c9b0b33b15e2993dba7697c4bf448551
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-02-20 15:49:17 +0300

    Find Min and max Sum HackerRank :smiley:

--- ds/test.php
+++ ds/test.php
@@ -1,16 +1,14 @@
 <?php
 
-function sum_small(array $arr)
-{
-    arsort($arr);
-    $min_value = array_shift($arr);
-    $sum_min = array_sum($arr);
-    array_unshift($arr, $min_value);
-    array_pop($arr);
-    $sum_max = array_sum($arr);
-    echo $sum_max . "\n";
-    echo $sum_min . "\n";
-    return $sum_max + $sum_min;
+// Complete the miniMaxSum function below.
+function miniMaxSum($arr) {
+    $sum = array();
+
+    for($i =0; $i < count($arr); $i++) {
+       $sum[] = array_sum($arr) - $arr[$i];
+    }
+
+     echo min($sum) ." " .max($sum);
 }
 
-echo "Sum -->" . sum_small([1, 2, 3, 4, 5, 6, 7]);
+(miniMaxSum([5, 5, 5, 5, 5]));
